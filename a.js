3e0c38a486ecc15bcd3d5548de75b926387049d0
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-18 11:05:59 +0530

    updated files

--- a.js
+++
@@ -1,8 +0,0 @@
-// var minElement = function (nums) {
-//   let arr = nums.map(num=>num.toString().split('').map(Number).reduce((acc,val)=>acc+val,0))
-  
-// return (...arr)
-  
-// }; 
-
-// console.log(minElement([23, 43, 54, 51]));
