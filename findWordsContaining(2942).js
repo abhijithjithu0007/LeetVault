53d2467cea1346660031d558d85472b9565d249e
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-20 20:23:22 +0530

    one problem resolved

---
+++ findWordsContaining(2942).js
@@ -0,0 +1,12 @@
+var findWordsContaining = function (words, x) {
+  let arr = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) {
+      arr.push(i);
+    }
+    
+  }
+  return arr;
+};
+
+console.log(findWordsContaining(["leet", "code"], "e"));
