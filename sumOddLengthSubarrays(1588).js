20c4471aecc50fd98e3bf734d2d5c08ed9c1c6c6
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-21 20:59:14 +0530

    another problem resolved

--- sumOddLengthSubarrays(1588).js
+++ sumOddLengthSubarrays(1588).js
@@ -1,5 +1,16 @@
-var sumOddLengthSubarrays = function(arr) {
-    
-};
+// var sumOddLengthSubarrays = function (arr) {
+//   let sum = [];
+//   let red = arr.reduce((val, ind) => val + ind, 0);
+//   for (let i = 0; i < arr.length - 2; i++) {
+//     sum.push(arr[i] + arr[i + 1] + arr[i + 2]);
+//   }
 
-console.log(sumOddLengthSubarrays([1,4,2,5,3]));
+// let sumred = sum.reduce((val,ind)=>val+ind,0)
+// let arrRed = arr.reduce((val,ind)=>val+ind,0)
+
+// return red+sumred+arrRed
+
+  
+// };
+
+// console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3]));
