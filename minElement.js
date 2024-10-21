e43eccc41f877baa9f90e4729789da545c878e07
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-21 20:29:46 +0530

    another problem resolved

--- minElement.js
+++ minElement.js
@@ -1,7 +1,7 @@
 var minElement = function (nums) {
   let arr = nums.map(num=>num.toString().split('').map(Number).reduce((acc,val)=>acc+val,0))
   
-return (...arr)
+return [...arr]
   
 }
 console.log(minElement([23, 43, 54, 51]));
