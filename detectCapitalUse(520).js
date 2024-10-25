3ab7362dfca9470296c18a8d0902f071270e189d
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-25 16:27:53 +0530

    pendin 389

---
+++ detectCapitalUse(520).js
@@ -0,0 +1,16 @@
+// var detectCapitalUse = function(word) {
+//     let upper = word.toUpperCase()
+//     let lower = word.toLowerCase()
+//     let mix = word[0].toUpperCase()
+    
+
+//     if(word===upper||word===lower||word[0]===mix){
+//         return true
+//     }else {
+//         return false
+//     }
+
+    
+// };
+
+// console.log(detectCapitalUse("FlaG"));
---
+++ findTheDifference(389).js
@@ -0,0 +1,15 @@
+// var findTheDifference = function(s, t) {
+//     let arr =[]
+//     let one = s.split('')
+//     let two = t.split('')
+//     for(let i=0;i<two.length;i++){
+//         if (two.indexOf(one[i]) == -1) {
+//             arr.push(two[i]);
+//           }
+//     }
+
+    
+//     return arr.toString()
+// };
+
+// console.log(findTheDifference("abcd","abcde"));
