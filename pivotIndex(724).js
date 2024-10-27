89fb24c9124cd004ef0323281dd20c284f9b47af
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-27 20:26:47 +0530

    resolved 724

--- pivotIndex(724).js
+++ pivotIndex(724).js
@@ -1,6 +1,16 @@
 var pivotIndex = function (nums) {
+  let red = nums.reduce((val, ind) => val + ind, 0);
+  let left = 0;
 
+  for (let i = 0; i < nums.length; i++) {
+    if (left === red - left - nums[i]) {
+      return i;
+    }
+    left+=nums[i]
+  }
+  return -1
 };
 
-[1, 7, 3, 6, 5, 6]
-[6, 5, 6, 3, 7, 1]
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+
+[1, 7, 3, 6, 5, 6][(6, 5, 6, 3, 7, 1)];
