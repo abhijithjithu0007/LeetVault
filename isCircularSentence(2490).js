00073a3b079451e7389dfd20d3cef45be717aa23
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-10-30 18:25:10 +0530

    resolved 2490

---
+++ isCircularSentence(2490).js
@@ -0,0 +1,35 @@
+var isCircularSentence = function (sentence) {
+  let spl = sentence.split(" ");
+
+
+  if(spl[0][0]!==spl[spl.length - 1].slice(-1)){
+    return false
+  }
+
+  if (spl.length == 1) {
+    for (let i = 0; i < spl.length; i++) {
+      let lastLetter = spl[i][spl[i].length - 1];
+      let check = spl[i][0];
+
+      if (lastLetter !== check) {
+        return false;
+      }
+    }
+      } else {
+        for (let i = 0; i < spl.length - 1; i++) {
+          let lastLetter = spl[i][spl[i].length - 1];
+          let check = spl[i + 1][0];
+
+          if(lastLetter!==check){
+              return false
+          }
+          
+          
+        }
+
+    
+  }
+  return true
+};
+
+console.log(isCircularSentence("Leetcode eisc cool"));
