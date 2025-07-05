598be6fa5450dae1507e26ef04ad6436c7b47ae0
Abhijith V <142149141+abhijithjithu0007@users.noreply.github.com>
2025-07-05 14:22:34 +0530

    resolved generateTag(3582)

---
+++ generateTag(3582).js
@@ -0,0 +1,8 @@
+const generateTag = caption => {
+    const words = caption.match(/[a-zA-Z]+/g) || [];
+    const camel = words.map((w, i) => !i 
+            ? w.toLowerCase() 
+            : w[0].toUpperCase() + w.slice(1).toLowerCase()
+    ).join('');
+    return ('#' + camel).slice(0, 100);
+};
